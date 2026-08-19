2262187f722e9291dbd67f224f0d9c7fdae641d3
Hassan Ayinde <ayindehassan776@gmail.com>
2026-08-19 16:37:06 -0400

    added corsheader to settings

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist',  
+    'rest_framework_simplejwt.token_blacklist', 
+    'corsheaders',   
     'users',
     'articles',  
     'programmes',
@@ -59,6 +60,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -178,3 +180,8 @@ if 'test' in sys.argv:
 
 
 CONTACT_FORM_RECIPIENT = os.environ.get("CONTACT_FORM_RECIPIENT", "hello@theclarridge.org")
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
